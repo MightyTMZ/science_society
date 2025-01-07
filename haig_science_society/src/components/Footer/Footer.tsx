56b1b6b8a74984022a73e9c66b8905069c870d21
Tom Zhang <tommyawesome08@gmail.com>
2025-01-06 21:55:22 -0500

    footer links updated and optimized

--- haig_science_society/src/components/Footer/Footer.tsx
+++ haig_science_society/src/components/Footer/Footer.tsx
@@ -1,37 +1,41 @@
 import React from "react";
 import styles from "./Footer.module.css"; // Add CSS styles for the footer
 import { FaInstagram, FaGlobe, FaEnvelope } from "react-icons/fa"; // Font Awesome icons
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.linkContainer}>
-        <a
-          href="https://instagram.com/yourprofile"
+        <Link
+          href="https://www.instagram.com/haigsciencesociety/"
           target="_blank"
           rel="noopener noreferrer"
           className={styles.footerLink}
         >
           <FaInstagram className={styles.icon} />
           <span>Instagram</span>
-        </a>
-        <a
-          href="https://yourwebsite.com"
+        </Link>
+        <Link
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
           className={styles.footerLink}
         >
           <FaGlobe className={styles.icon} />
           <span>Website</span>
-        </a>
-        <a href="mailto:your-email@example.com" className={styles.footerLink}>
+        </Link>
+        <Link
+          href="mailto:earlhaigsciencesociety@gmail.com"
+          className={styles.footerLink}
+        >
           <FaEnvelope className={styles.icon} />
           <span>Email</span>
-        </a>
+        </Link>
       </div>
       <p className={styles.disclaimer}>
-        Designed by Angela Ho <br />
-        Built by the Tom Zhang
+        Designed by <strong>Angela Ho</strong> <br />
+        Built by the <strong>Tom Zhang</strong>
       </p>
     </footer>
   );
