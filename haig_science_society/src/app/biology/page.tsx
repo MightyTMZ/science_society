5eb70b7a6d9d549f6fb8298b3d33e431dc319b4a
Tom Zhang <tommyawesome08@gmail.com>
2025-02-19 13:56:24 -0500

    HOSA added

--- haig_science_society/src/app/biology/page.tsx
+++ haig_science_society/src/app/biology/page.tsx
@@ -15,8 +15,12 @@ const Biology = () => {
         <AppBanner title={departmentName} imageURL={BioBanner} />
       </div>
       <p className="mt-4 ml-4">
-        <strong>Weekly Meeting (Biology)</strong>: Wednesday at Lunch (11:40-12:40) in room
-        342
+        <strong>Weekly Meeting (Biology)</strong>: Wednesday at Lunch
+        (11:40-12:40) in room 342
+      </p>
+      <p className="mt-4 ml-4">
+        <strong>Weekly Meeting (HOSA)</strong>: Friday at Lunch (11:40-12:40) in
+        room 342
       </p>
 
       <div className={styles.announcementsContainer + "container lg"}>
